4b8b48fdb265d7a3ae1281ff34f528a90576151f
Nidhin Paul <nidhinpaul2000@gmail.com>
2019-11-09 09:28:49 +0000

    initial release

--- onthego/onthego/src/main/java/com/project/onthego/controller/UserController.java
+++ onthego/onthego/src/main/java/com/project/onthego/controller/UserController.java
@@ -1,5 +1,10 @@
 package com.project.onthego.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/api/user")
 public class UserController {
 
 }
