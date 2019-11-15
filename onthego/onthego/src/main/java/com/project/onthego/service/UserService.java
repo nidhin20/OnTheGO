15d04a099169e072a6988fa3ce38d23a20517e13
Nidhin Paul <nidhinpaul2000@gmail.com>
2019-11-15 17:44:15 +0000

    Initial release

--- onthego/onthego/src/main/java/com/project/onthego/service/UserService.java
+++ onthego/onthego/src/main/java/com/project/onthego/service/UserService.java
@@ -2,21 +2,29 @@
 package com.project.onthego.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.project.onthego.model.UserMembership;
 import com.project.onthego.model.Users;
+import com.project.onthego.repository.UserMembershipRepository;
 import com.project.onthego.repository.UserRepository;
 
 
 @Service
 public class UserService {
 	@Autowired
-    private UserRepository repository;
-	
+    private UserRepository userrepository;
+	@Autowired
+    private UserMembershipRepository usermembershiprepository;	
 	public List<Users> getAll(){
-		return repository.findAll();
+		return userrepository.findAll();
 	}
+	public Optional<UserMembership> getAllLinkedcards(int userid){
+		return usermembershiprepository.findById(userid);
+	}
+		
 		
 	}
