8ac14881128fd6a7d50d29873fea4e86333bd718
Jithin <jithinraju95@gmail.com>
2019-11-15 20:21:01 +0000

    EMAIL

--- onthego/onthego/src/main/java/com/project/onthego/service/UserService.java
+++ onthego/onthego/src/main/java/com/project/onthego/service/UserService.java
@@ -2,35 +2,25 @@
 package com.project.onthego.service;
 
 import java.util.List;
-import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.project.onthego.model.UserMembership;
-import com.project.onthego.repository.UserMembershipRepository;
-
-import com.project.onthego.DTO.UserDto;
-import com.project.onthego.DTO.UserSignUpDto;
+import com.project.onthego.dto.UserDto;
+import com.project.onthego.dto.UserSignUpDto;
 import com.project.onthego.model.User;
 import com.project.onthego.repository.UserRepository;
+import com.project.onthego.validator.EmailValidation;
 
 
 
 @Service
 public class UserService {
 	@Autowired
-	private UserRepository userRepository;
-	@Autowired
-    private UserMembershipRepository usermembershiprepository;	
-	public List<User> getAll(){
-		return userRepository.findAll();
-	}
-	public Optional<UserMembership> getAllLinkedcards(int userid){
-		return usermembershiprepository.findById(userid);
-	}
-    
+    private UserRepository userRepository;
 	
 	 /*@Autowired
 	    private PasswordEncoder passwordEncoder;*/
@@ -45,6 +35,11 @@ public class UserService {
 	public UserDto addUser( UserSignUpDto userSignUpDto) {
 		User user = new User() ;
 		//user.setPassword(passwordEncoder.encode(userSignUpDto.getPassword()));
+		EmailValidation e=new EmailValidation();
+		if(userRepository.validbyEmail(userSignUpDto.getEmail())!=1)
+		{
+		int valid=e.validateEmail(userSignUpDto.getEmail());
+		if(valid==1) {
 		user.setPassword(userSignUpDto.getPassword());
         user.setName(userSignUpDto.getName());
         user.setDob(userSignUpDto.getDob());
@@ -58,7 +53,9 @@ public class UserService {
        
 
         return userDto;
+        }
+		}
+		return null;
 	}
-		
 		
 	}
