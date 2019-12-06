baf622c0f7500a6ad692902a6b12bfce9cbc6a51
Nidhin Paul <nidhinpaul2000@gmail.com>
2019-12-06 15:53:34 +0000

    Command pattern modification

---
+++ onthego/onthego/src/main/java/com/project/onthego/service/commandswitch.java
@@ -0,0 +1,17 @@
+package com.project.onthego.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+@Service
+public class commandswitch {
+	@Autowired
+	private Command command_card;
+	public void setcommandswitch(Command Link)
+	{
+		command_card=Link;
+	}
+	public void commandswitchservice() {
+		command_card.execute();
+	}
+
+}
